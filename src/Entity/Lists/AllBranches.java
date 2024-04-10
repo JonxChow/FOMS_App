34daebb578e12a01c1c23efff3939bb7e7695516
Anish-Gholap <anishgholap@gmail.com>
2024-04-10 15:01:36 +0800

    Login system, main and allBranches class

---
+++ src/Entity/Lists/AllBranches.java
@@ -0,0 +1,15 @@
+package Entity.Lists;
+
+import Entity.Branch.Branch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllBranches {
+
+    private List<Branch> allBranches;
+
+    public AllBranches() {
+        this.allBranches = new ArrayList<>();
+    }
+}
